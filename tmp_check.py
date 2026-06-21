50479de2cc116a55a31ec2a06c749059faf02d25
vk2610 <vedantkumbhar82@gmail.com>
2026-06-21 21:31:11 +0530

    description added

--- tmp_check.py
+++ tmp_check.py
@@ -1,3 +1,6 @@
+# !/usr/bin/env python
+# Description : This script loads a PyTorch model from a specified path, extracts the shapes of the parameters related to the classifier, and saves this information in a JSON file.
+
 import torch, json
 sd = torch.load('model/model.pth', map_location='cpu')
 res = {}
